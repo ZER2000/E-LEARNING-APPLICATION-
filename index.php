dc0c647f92d52d585c2a442bdebd68619b90dd5a
zerzkhane <zezkhanemourad@gmail.com>
2022-02-03 10:20:27 +0100

    vs

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="bootstrap.min.css">
   <link rel="stylesheet" href="style.css">
-  <title>LOG IN</title>
+  <title>Document</title>
 </head>
 <body>
     <main class="signin d-flex justify-content-center align-items-center">
